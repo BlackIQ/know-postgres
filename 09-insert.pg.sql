611027786c6023002d60d2415df86164d4a0f1c3
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-13 00:57:58 +0330

    TABLE: Using SERIAL to have auto inc

---
+++ 09-insert.pg.sql
@@ -0,0 +1,5 @@
+-- How to insert many rows?
+-- Put lines after lines!?
+-- Well, do not do that!
+
+INSERT INTO people (first_name, last_name) VALUES ('Amirhossein', 'Mohammadi'), ('Kasra', 'Roushan'), ('Mia', 'Khalifa');
