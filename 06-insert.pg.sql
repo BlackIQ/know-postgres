1f581e9aa79c0866c89cafb95757381d3f381174
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-13 00:43:37 +0330

    INSERT: Insert in specify columns

---
+++ 06-insert.pg.sql
@@ -0,0 +1,11 @@
+-- Inserting is easy
+-- But what if we need to just specify 2 columns?
+-- Should I enter rest NULL!?
+-- This is how we only select into each column we want
+
+INSERT INTO people (id, age, first_name, last_name) VALUES (2, 24, 'Amirali', 'Mohammadi');
+
+-- As you can see, I even say id, age
+-- In table structure order they are not
+-- after each other. But now we did it!
+-- See you at 07
