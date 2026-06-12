4101f9e8fa48cfdcd3d3ab6234530a920170f856
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-13 01:19:48 +0330

    DELETE: Simple and Easy

---
+++ 14-delete.pg.sql
@@ -0,0 +1,11 @@
+-- Can you remember part 13?
+-- If not, check that out
+-- UPDATE and DELETE are very same
+
+-- DELETE the guy with ID 9
+DELETE FROM people WhERE id = 9;
+
+-- DELETE all people with job FrontEnd
+DELETE FROM people WHERE job = 'FrontEnd'
+
+-- See you at 15
