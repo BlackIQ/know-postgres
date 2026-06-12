f020a3033b856755a72b86b8995e2a38f8d1bfc7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-06-13 00:36:50 +0330

    INSERT: How to insert a row

---
+++ 04-insert.pg.sql
@@ -0,0 +1,17 @@
+-- INSERT. The statement to enter data
+-- Alright? At 01,02 we created a table
+-- At 03 we tried to get data
+-- But we have no data. What to do?
+-- Insert a bit!
+
+INSERT INTO people VALUES (1, "Amirhossein", "Mohammadi", 24, "Network Administrator", FALSE);
+
+-- Look at this syntax deeply
+-- We said: intert in a table which name is people
+-- With values we are telling you.
+-- Enter values in order column names
+-- So, now we added a row!
+-- Amirhossein Mohammadi has 24 years old who is a
+-- Network Administrator that now is looking for work
+-- with ID 1
+-- See you at 05
